68cfa1dc4826a29c8009126f228ff0f2b93ab53d
Richard Hasselgren <richard.hasselgren@elev.it-gymnasiet.se>
2015-05-18 10:33:04 +0200

    Login works now.
    Main.slim redirect is in the works, but a sign up is needed before.

--- app.rb
+++ app.rb
@@ -6,8 +6,17 @@ class App < Sinatra::Base
   end
 
   get '/login' do
-    
     slim :login
+  end
+
+  post '/login' do
+    user = User.first(uname: params['uname'].downcase)
+    if user && user.pass == params['pass']
+      session[:user] = user.id
+      redirect '/main'
+    else
+      redirect '/'
+    end
   end
 
 end
